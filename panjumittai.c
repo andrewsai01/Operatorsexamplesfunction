218d443fc2116a0450fb1623efee05f92cb6bfbe
Vinu Andrews S <Andrewsai01@brightskills.com>
2024-01-03 13:26:51 +0530

    panjumittai updated

--- panjumittai.c
+++ panjumittai.c
@@ -1,6 +1,7 @@
 # include <stdio.h>
 int main ()
 {
-    printf ("Welcome to Panjumittai Channel");
+    printf ("\nWelcome to Panjumittai Channel\n");
+    printf ("\nLike karo Share karo \nsubscribe karo and press the bell icon to get notification regularly\n");
     return 0;
 }
