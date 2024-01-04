2c6aec8234b8676f663e17925d69e6e6d73e82f1
Vinu Andrews S <Andrewsai01@brightskills.com>
2024-01-04 12:11:36 +0530

    programe modulus

---
+++ modulus.c
@@ -0,0 +1,23 @@
+ #include<stdio.h>
+int main ()
+{
+    int a;
+    int b=4;
+    int c;
+    printf("enter the value A");
+    scanf("%d",&a);
+    c=a%b;
+    if(c==0)
+    {
+        printf("the given value is even");
+    }
+    else 
+    {
+        printf("the given value is odd");
+
+    }
+ return 0;
+}
+
+      
+
---
+++ programe.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+{
+    int a;
+    printf("enter the a");
+    scanf("%d",&a);
+    if (a>=0)
+    {
+    printf("given the is positive");
+    }
+    else if (a<=0)
+    {
+        printf("given the is negative");
+    }
+    return 0;
+}
+
+
